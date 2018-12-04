9e6537d59c14755df19d50ce1b1eda94364dab3f
skuehlewind <Sebastian.kuehlewind@uni-oldenburg.de>
2018-12-04 20:07:42 +0100

    Füge neues Skript zum Snychronisieren der Daten ein
    DataScript enthält Spielerposition, Blickrichtung und Größe des Labyrinths. Das Skript wird synchronisiert.

---
+++ AMazeMe Smartphone/Assets/DataScript.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataScript : MonoBehaviour {
+    public int value0, value1, value2, value3, value4, value5;
+}
---
+++ AMazeMe/Assets/DataScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataScript : MonoBehaviour {
+
+    public Vector3 PlayerPosVec;
+    public Quaternion PlayerRotVec;
+    public int mazeRows, mazeColumns;
+
+    private MazeLoader MazeLoaderScript;
+    private RightController RightControllerScript;
+    private Transform ViveCameraRotation;
+    private bool PlayerPosChanged;
+
+    private void FixedUpdate()
+    {
+        // Update info about maze size 
+        if (MazeLoaderScript == null)
+        {
+            MazeLoaderScript = GameObject.Find("[CameraRig]").GetComponent<MazeLoader>();
+            this.mazeRows = MazeLoaderScript.mazeRows;
+            this.mazeColumns = MazeLoaderScript.mazeColumns;
+        }
+
+        // Update player position
+        if (RightControllerScript == null)
+        {
+            GameObject NonHmdController = GameObject.Find("NonHmdController");
+            if (NonHmdController != null)
+            {
+                RightControllerScript = NonHmdController.GetComponent<RightController>();
+            }
+        } else
+        {
+            PlayerPosChanged = RightControllerScript.playerX != this.PlayerPosVec.x || RightControllerScript.playerZ != this.PlayerPosVec.z;
+
+            if (PlayerPosChanged)
+            {
+                this.PlayerPosVec = new Vector3(RightControllerScript.playerX, 0, RightControllerScript.playerZ);
+            }
+        }
+
+        // Update player camera rotation
+        if (ViveCameraRotation == null)
+        {
+            ViveCameraRotation = GameObject.Find("Camera (eye)").GetComponent<Transform>();
+        } else
+        {
+            this.PlayerRotVec = ViveCameraRotation.rotation;
+        }
+
+    }
+}
