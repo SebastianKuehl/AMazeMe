67c0c40ffbe3c00ff51d11f7641e152d9e52910a
skuehlewind <Sebastian.kuehlewind@uni-oldenburg.de>
2018-12-15 23:26:45 +0100

    Entferne Smartphone Projekt und ersetze es durch 2D Projekt Tablet
    Das neue Projekt verfügt nun über eine Tilemap, die eine ansprechende Ansicht des Labyrinths generiert.

--- AMazeMe Smartphone/Assets/DataScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class DataScript : MonoBehaviour {
-    public Vector3 PlayerPosVec;
-    public Quaternion PlayerRotVec;
-	public int mazeRows, mazeColumns;
-}
---
+++ AMazeMe Tablet/Assets/Scripts/MainScript.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour {
+
+    public Tilemap tilemap;
+    public Tile coveredFloorTile;
+    public Tile allWallTile;
+    public Tile allPillarTile;
+    public Tile twoWallWestEastTile;
+    public Tile twoWallSouthNorthTile;
+    public Tile threeWallNorthTile;
+    public Tile threeWallSouthTile;
+    public Tile threeWallEastTile;
+    public Tile threeWallWestTile;
+    public Tile tCornerNorthTile;
+    public Tile tCornerSouthTile;
+    public Tile tCornerEastTile;
+    public Tile tCornerWestTile;
+    public Tile cornerSouthEastTile;
+    public Tile cornerWestNorthTile;
+    public Tile cornerNorthEastTile;
+    public Tile cornerWestSouthTile;
+
+    private GameObject gameObj;
+    private MazeDataScript mazeDataScript;
+    private PlayerPositionScript playerPositionScript;
+    private PlayerRotationScript playerRotationScript;
+
+    private int mazeRows, mazeColumns;
+    private int[,] mazeStructure;
+    private List<Vector2> playerPositionList;
+    private Vector2 newPosition, lastPosition;
+
+    void Start () {
+        gameObj = GameObject.Find("GameObject");
+        playerPositionList = new List<Vector2>();
+        newPosition = new Vector2(0, 0);
+	}
+	
+	void Update () {
+        // Handle the maze data before the others in order to generate the main floor first
+        if (!HandleMazeData()) {
+            return;
+        }
+        HandlePlayerMovement();
+        HandlePlayerRotation();
+
+        UpdateMap();
+    }
+
+    private bool HandleMazeData() {
+        if (!mazeDataScript) {
+            mazeDataScript = gameObj.GetComponentInChildren<MazeDataScript>();
+            if (!mazeDataScript) {
+                return false;
+            }
+            mazeRows = mazeDataScript.mazeRows;
+            mazeColumns = mazeDataScript.mazeColumns;
+            mazeStructure = new int[mazeRows * 3, mazeColumns * 3];
+            for (int i = 0; i < mazeRows * 3; i++) {
+                for (int j = 0; j < mazeColumns * 3; j++) {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), coveredFloorTile);
+                }
+            }
+        }
+        return true;
+    }
+
+    private void HandlePlayerMovement() {
+        if (!playerPositionScript) {
+            playerPositionScript = gameObj.GetComponentInChildren<PlayerPositionScript>();
+            if (!playerPositionScript) {
+                return;
+            }
+        }
+        // Add the latest position to the list if not done before
+        newPosition.x = playerPositionScript.playerX;
+        newPosition.y = playerPositionScript.playerZ; // playerZ because the PC application is in 3D and Y is up/down
+        if (playerPositionList.Count <= 0) {
+            lastPosition = newPosition;
+            Debug.Log("Adding Position " + newPosition);
+            playerPositionList.Add(newPosition); // TODO Check that its not referenced and the values are kept
+        } else {
+            lastPosition = playerPositionList[playerPositionList.Count - 1];
+            if (lastPosition.x != newPosition.x || lastPosition.y != newPosition.y) {
+                playerPositionList.Add(newPosition);
+                Debug.Log("Adding Position " + newPosition);
+            }
+        }
+
+        // Update the path inbetween the last and current position
+        UpdatePath(lastPosition, newPosition);
+    }
+
+    private void UpdatePath(Vector2 lastPos, Vector2 newPos) {
+        if (lastPos.x >= mazeRows || lastPos.y >= mazeColumns || newPos.x >= mazeRows || newPos.y >= mazeColumns) {
+            Debug.LogError("Values of some positions are larger than existing matrix.");
+            return;
+        }
+        // Make new position show in map
+        mazeStructure[(int)newPos.x * 3 + 1, (int)newPos.y * 3 + 1] = 1;
+
+        // If positions are unequal then update path depending on direction
+        if (lastPos.x != newPos.x || lastPos.y != newPos.y) {
+            // Player went north
+            if (lastPos.x == newPos.x && lastPos.y < newPos.y) {
+                mazeStructure[(int)lastPos.x * 3 + 1, (int)lastPos.y * 3 + 2] = 1;
+                mazeStructure[(int)lastPos.x * 3 + 1, (int)lastPos.y * 3 + 3] = 1;
+            }
+            // Player went south
+            if (lastPos.x == newPos.x && lastPos.y > newPos.y) {
+                mazeStructure[(int)lastPos.x * 3 + 1, (int)lastPos.y * 3] = 1;
+                mazeStructure[(int)lastPos.x * 3 + 1, (int)lastPos.y * 3 - 1] = 1;
+            }
+            // Player went east
+            if (lastPos.x < newPos.x && lastPos.y == newPos.y) {
+                mazeStructure[(int)lastPos.x * 3 + 2, (int)lastPos.y * 3 + 1] = 1;
+                mazeStructure[(int)lastPos.x * 3 + 3, (int)lastPos.y * 3 + 1] = 1;
+            }
+            // Player went west
+            if (lastPos.x > newPos.x && lastPos.y == newPos.y) {
+                mazeStructure[(int)lastPos.x * 3, (int)lastPos.y * 3 + 1] = 1;
+                mazeStructure[(int)lastPos.x * 3 - 1, (int)lastPos.y * 3 + 1] = 1;
+            }
+        }
+    }
+
+    private void HandlePlayerRotation() {
+        if (!playerRotationScript) {
+            playerRotationScript = gameObj.GetComponentInChildren<PlayerRotationScript>();
+        }
+
+    }
+
+    private void UpdateMap() {
+        // Go through mazeStructure and place needed tile for each point
+        for (int i = 1; i < mazeRows * 3 - 1; i++) {
+            for (int j = 1; j < mazeColumns * 3 - 1; j++) {
+                // Found a tile that has been visited
+                if (mazeStructure[i, j] == 1) {
+                    tilemap.SetTile(new Vector3Int(i, j, 0), GetTile(i, j));
+                }
+            }
+        }
+    }
+
+    private Tile GetTile(int x, int y) {
+        string northVisited = VisitedNorth(x, y) ? "North " : "No ";
+        string southVisited = VisitedSouth(x, y) ? "South " : "No ";
+        string eastVisited = VisitedEast(x, y) ? "East " : "No ";
+        string westVisited = VisitedWest(x, y) ? "West" : "No";
+        string result = northVisited + southVisited + eastVisited + westVisited;
+        switch(result) {
+            case "North South East West":
+                return allPillarTile;
+            case "North South East No":
+                return tCornerWestTile;
+            case "North South No West":
+                return tCornerEastTile;
+            case "North South No No":
+                return twoWallSouthNorthTile;
+            case "North No East West":
+                return tCornerSouthTile;
+            case "North No East No":
+                return cornerNorthEastTile;
+            case "North No No West":
+                return cornerWestNorthTile;
+            case "North No No No":
+                return threeWallNorthTile;
+            case "No South East West":
+                return tCornerNorthTile;
+            case "No South East No":
+                return cornerSouthEastTile;
+            case "No South No West":
+                return cornerWestSouthTile;
+            case "No South No No":
+                return threeWallSouthTile;
+            case "No No East West":
+                return twoWallWestEastTile;
+            case "No No East No":
+                return threeWallEastTile;
+            case "No No No West":
+                return threeWallWestTile;
+            case "No No No No":
+                return allWallTile;
+            default:
+                return allWallTile;
+        }
+    }
+
+    private bool VisitedNorth(int x, int y) {
+        return mazeStructure[x, y + 1] == 1;
+    }
+
+    private bool VisitedSouth(int x, int y) {
+        return mazeStructure[x, y - 1] == 1;
+    }
+
+    private bool VisitedEast(int x, int y) {
+        return mazeStructure[x + 1, y] == 1;
+    }
+
+    private bool VisitedWest(int x, int y) {
+        return mazeStructure[x - 1, y] == 1;
+    }
+}
