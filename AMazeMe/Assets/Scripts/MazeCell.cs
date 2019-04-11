632eaf8ce57bb26a873a558471efcfc121c7c0e2
skuehlewind <Sebastian.kuehlewind@uni-oldenburg.de>
2019-04-11 18:43:51 +0200

    Remove old versions due to an unsolveable error

--- AMazeMe/Assets/Scripts/MazeCell.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-public class MazeCell {
-	public bool visited = false;
-	public GameObject northWall, southWall, eastWall, westWall, floor;
-	public bool northWallExists = true, southWallExists = true, eastWallExists = true, westWallExists = true;
-}
